49cefdc2c8e9b399f06202cf7660af054c5e5150
malayshravan727 <malayshravan727@gmail.com>
2021-07-08 18:40:05 +0530

    component Life cycle methods

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -14,29 +14,55 @@
 import React from "react";
 
 class User extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            planet:"earth",
-        };
-    console.log("Hey Im from constructor()");
-    }
-    componentDidMount() {
-        this.setState({planet: "Mars"});
-    }
-    render() {
-        console.log("Hey Im from render()");
-        //view
-        //logic
-       return (
-        <div>
-            <h1>{this.props.name}</h1>
-            <p>{this.props.description}</p>
-            <h4>{this.state.planet}</h4>
-        </div>
-       );
-    }
+    this.state = {
+      planet: "earth",
+    };
+    console.log("Hey I'm from Constructor");
+  }
+
+  componentDidMount() {
+    this.setState({ planet: "jupiter" });
+  }
+
+  // static getDerivedStateFromProps(prop, state) {
+  //   console.log("I'm from get derived state from props");
+  //   return { planet: "Jupiter" };
+  // }
+
+  shouldComponentUpdate(nextProp, nextState) {
+    console.log("from shouldComponentUpdate");
+    console.log({
+      nextProp,
+      nextState,
+    });
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProp, prevState) {
+    console.log("from getSnapshotBeforeUpdate");
+    console.log({ prevProp, prevState });
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  render() {
+    console.log("Hey Im from render()");
+    return (
+      <div>
+        <h1> {this.props.name} </h1>
+        <p> {this.props.description} </p>
+        <h4>{this.state.planet}</h4>
+      </div>
+    );
+  }
 }
 
 export default User;
+
+// state
